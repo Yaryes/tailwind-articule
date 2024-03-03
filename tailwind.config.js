596ed2d7b0e7b67f67f343895822b900d37a91dc
Yaryes <carlosantonioyaryes@gmail.com>
2024-03-03 20:53:43 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Indicando que exporte los archivos con ese tipo de archivo!! {{./*.html}}
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
